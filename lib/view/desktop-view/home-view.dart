6afab8fb7eb355bdd951444b38c903d75f26170d
Areeba Iqbal <areeba.seo05@gmail.com>
2025-01-04 10:36:32 +0500

    add dart files

---
+++ lib/view/desktop-view/home-view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomeViewDesktop extends StatefulWidget {
+  const HomeViewDesktop({super.key});
+
+  @override
+  State<HomeViewDesktop> createState() => _HomeViewDesktopState();
+}
+
+class _HomeViewDesktopState extends State<HomeViewDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(height: double.infinity,, width:  double.infinity, color: Colors.green,)
+    );
+  }
+}
